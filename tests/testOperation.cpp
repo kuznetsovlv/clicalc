1731afed848e66d892e519d37dd70cce4299a099
Leonid Kuznetov <kuznetsovlv1981@gmail.com>
2020-02-24 02:05:15 +0300

    Tested operations

--- tests/testOperation.cpp
+++ tests/testOperation.cpp
@@ -1,4 +1,5 @@
 #include "test.h"
+#include "cmath"
 
 #include "../src/stack.h"
 #include "../src/operation.h"
@@ -9,19 +10,28 @@ using namespace std;
 class TestOperation : public TestFixture
 {
 	CPPUNIT_TEST_SUITE(TestOperation);
+	CPPUNIT_TEST(testPower);
+	CPPUNIT_TEST(testMult);
+	CPPUNIT_TEST(testDiv);
 	CPPUNIT_TEST(testAddition);
 	CPPUNIT_TEST(testSubtraction);
+	CPPUNIT_TEST(testAbs);
 	CPPUNIT_TEST_SUITE_END();
 
 void process(Operation*, long double, long double, long double);
+void process(Operation*, long double, long double);
 
 public:
 	void setUp();
 	void tearDown();
 
 protected:
+	void testPower();
+	void testMult();
+	void testDiv();
 	void testAddition();
 	void testSubtraction();
+	void testAbs();
 };
 
 void TestOperation::process(Operation* op, long double a, long double b, long double expected)
@@ -32,10 +42,56 @@ void TestOperation::process(Operation* op, long double a, long double b, long do
 	CPPUNIT_ASSERT(op->complete(stack) == expected);
 }
 
-void TestOperation::testAddition()
+void TestOperation::process(Operation* op, long double a, long double expected)
 {
 	Stack<long double> stack;
+	stack.push(a);
+	CPPUNIT_ASSERT(op->complete(stack) == expected);
+}
+
+void TestOperation::testPower()
+{
+	Operation *powOp = Operation::getOperation("^");
+	CPPUNIT_ASSERT(powOp->getPriority() == primary);
+
+	long double a = 12.04, b = 18.3;
+	process(powOp, a, b, pow(a, b));
+
+	a = 3.1023;
+	b = -.123;
+	process(powOp, a, b, pow(a, b));
+}
+
+void TestOperation::testMult()
+{
+	Operation *mult = Operation::getOperation("*");
+	CPPUNIT_ASSERT(mult->getPriority() == secondary);
+
+	long double a = 5.23, b = 0.45;
+	process(mult, a, b, a * b);
+
+	a = 23.4;
+	b = -14.8903;
+	process(mult, a, b, a * b);
+}
+
+void TestOperation::testDiv()
+{
+	Operation *div = Operation::getOperation("/");
+	CPPUNIT_ASSERT(div->getPriority() == secondary);
+
+	long double a = 14.23, b = 12.42;
+	process(div, a, b, a / b);
+
+	a = -1.23;
+	b = 0.14;
+	process(div, a, b, a / b);
+}
+
+void TestOperation::testAddition()
+{
 	Operation *add = Operation::getOperation("+");
+	CPPUNIT_ASSERT(add->getPriority()  == last);
 
 	long double a = 12.03, b = 14.1;
 	process(add, a, b, a + b);
@@ -47,8 +103,8 @@ void TestOperation::testAddition()
 
 void TestOperation::testSubtraction()
 {
-	Stack<long double> stack;
 	Operation *sub = Operation::getOperation("-");
+	CPPUNIT_ASSERT(sub->getPriority() == last);
 
 	long double a = 12.03, b = 14.1;
 	process(sub, a, b, a - b);
@@ -56,6 +112,16 @@ void TestOperation::testSubtraction()
 	a = 12.03;
 	b = -12.0032;
 	process(sub, a, b, a - b);
+}
+
+void TestOperation::testAbs()
+{
+	Operation *absOp = Operation::getOperation("abs");
+	CPPUNIT_ASSERT(absOp->getPriority() == primary);
+
+	long double a = 12.34;
+	process(absOp, a, a);
+	process(absOp, -a, a);
 }
 
 
